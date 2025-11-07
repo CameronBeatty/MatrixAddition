67cac2fda5e2f08949a3a9ba0c9b793508c440e5
Cameron's PC <Cameron's PC@192.168.0.64>
2025-11-06 17:04:21 -0700

    Fully working, sorts all sizes of arrays, commented

--- MatrixAddition/src/main/Main.java
+++ MatrixAddition/src/main/Main.java
@@ -24,78 +24,106 @@ import java.util.Scanner;
 
 public class Main
 {
-	public static void main(String[] args) 
+	public static void main(String[] args) throws Exception
 	{
 		int[][] A = null;
 		int[][] B = null;
+		int row = 0;
+		int column = 0;
 		ThreadOperation operator1;
 		ThreadOperation operator2;
 		ThreadOperation operator3;
 		ThreadOperation operator4;
+		//Read In Data
 		try
 		{
-			File dataFile = new File("matrix1.txt");
+			//Create a new file
+			File dataFile = new File("matrix2.txt");
+			//Scan the file
 			Scanner fileReader = new Scanner(dataFile);
-			int row = Integer.parseInt(fileReader.next());
-			int column = Integer.parseInt(fileReader.next());
-			System.out.println(row + " " + column);
-			if(row % 2 == 0 && column % 2 == 0)
+			//The first two integers in the file
+			//are stored as the row and column and then
+			//we move to the next line
+			row = Integer.parseInt(fileReader.next());
+			column = Integer.parseInt(fileReader.next());
+			fileReader.nextLine();
+			//Create two new arrays to store
+			//the two separate arrays
+			A = new int[row][column];
+			B = new int[row][column];
+			//Read in array A, the first two integers of the
+			//file give us the size of the two arrays which
+			//are always the same so we use these values
+			//to determine when all of A and B has been read
+			for(int i = 0; i < row ; i++)
 			{
-				A = new int[row][column];
-				B = new int[row][column];
-				fileReader.nextLine();
-				for(int i = 0; i < row - 1; i++)
+				for(int j = 0; j < column; j++)
 				{
-					for(int j = 0; j < column - 1; j++)
-					{
-						A[i][j] = fileReader.nextInt();
-					}
-				}
-				for(int i = 0; i < row - 1; i++)
-				{
-					for(int j = 0; j < column - 1; j++ )
-					{
-						B[i][j] = fileReader.nextInt();
-					}
+					A[i][j] = fileReader.nextInt();
 				}
 			}
-			else if(row % 2 != 0 && column % 2 == 0 )
+			//Read in array B
+			for(int i = 0; i < row ; i++)
 			{
-				A = new int[row/2][column/2];
-				B = new int[(row/2) + 1][column/2];
-			}
-			else if(row % 2 == 0 && column % 2 != 0 )
-			{
-				A = new int[row/2][column/2];
-				B = new int[row/2][column/2 + 1];
-			}
-			
-			for(int i = 0; i < row - 1; i++)
-			{
-				for(int j = 0; j < column - 1; j++)
+				for(int j = 0; j < column; j++ )
 				{
-					System.out.print(A[i][j] + " ");
+					B[i][j] = fileReader.nextInt();
 				}
-				System.out.println();
 			}
-			for(int i = 0; i < row - 1; i++)
-			{
-				for(int j = 0; j < column - 1; j++ )
-				{
-					System.out.print(B[i][j] + " ");
-				}
-				System.out.println();
-			}
+			//Done scanning
+			fileReader.close();
 		}
+		//FILE ERROR
 		catch(IOException e)
 		{
 			System.out.println("FILE ERROR: NOT FOUND OR WRONG FORMAT");
 		}
-		
+		//Array to store sum of A and B
+		int[][] summedMatrix = new int[row][column];
+		//Print out A and B using print2DArray method
+		System.out.println("Original Arrays:");
+		print2DArray(A, row, column);
+		System.out.println();
+		print2DArray(B, row, column);
+		//Create 4 threads to sum A and B.
+		//summedMatrix is passed to all 4 threads,
+		//this is because summedMatrix is a 2D
+		//array which is an Object, and since it is
+		//an object it is passed by reference versus
+		//passed by value meaning we can manipulate the
+		//same array in all 4 threads through passing it in
+		//the constructor.
+		operator1 = new ThreadOperation(A, B, summedMatrix, ThreadOperation.Quadrant.Q1, row, column);
+		operator2 = new ThreadOperation(A, B, summedMatrix, ThreadOperation.Quadrant.Q2, row, column);
+		operator3 = new ThreadOperation(A, B, summedMatrix, ThreadOperation.Quadrant.Q3, row, column);
+		operator4 = new ThreadOperation(A, B, summedMatrix, ThreadOperation.Quadrant.Q4, row, column);
 		System.out.println();
 		
-		
-		
+		//start threads
+		operator1.start();
+		operator2.start();
+		operator3.start();
+		operator4.start();
+		//Print out the summed array
+		System.out.println("Summed Array:");
+		print2DArray(summedMatrix, row, column);
+	}//END of Main
+	
+	//takes a 2D array and the row and column amount
+	//as input. The entire 2D array is then output
+	//in a formated form
+	public static void print2DArray(int[][]array, int row, int column)
+	{
+		//loop through entire 2D array
+		//printing each element
+		for(int i = 0; i < row; i++)
+		{
+			for(int j = 0; j < column; j++)
+			{
+				System.out.printf("%-6d", array[i][j]);
+			}
+			System.out.println();
+		}
 	}
 
 }
--- MatrixAddition/src/main/ThreadOperation.java
+++ MatrixAddition/src/main/ThreadOperation.java
@@ -1,27 +1,141 @@
 package main;
-
-public class ThreadOperation  extends Thread
+//Handles the addition of two 2D arrays
+//by extending thread and breaking up the 
+//addition of the two arrays into four separate
+//quadrants
+public class ThreadOperation extends Thread
 {
-
+	//two arrays to add
 	int[][] A;
 	int[][] B;
-	
-	public enum quadrant
+	//the sum of the two arrays
+	int[][] sumArray;
+	Quadrant quadrant;
+	//row and column length from file
+	int row;
+	int column;
+	//enum to determine which quadrant
+	//is being added
+	public static enum Quadrant
 	{
 		Q1,
 		Q2,
 		Q3,
 		Q4,
 	}
-	
-	public ThreadOperation(int[][] A, int[][] B, String quadrant)
+	//constructor
+	public ThreadOperation(int[][] recievedA, int[][] recievedB, int[][] summedMatrix, Quadrant q, int row, int column)
 	{
-		
+		//A and B arrays assigned
+		this.A = recievedA;
+		this.B = recievedB;
+		//assign quadrant
+		this.quadrant = q;
+		//row and column from file
+		this.row = row;
+		this.column = column;
+		//pass the sumArray
+		this.sumArray = summedMatrix;
 	}
 	
+	@Override
+	//Adds up the threads assigned quadrant
+	//o fthe two arrays
 	public void run()
 	{
-		
-	}
-	
-}
+		//variables for specific quadrant being added
+		int quadrantRow;
+		int quadrantColumn;
+		//switch through the four quadrants
+		switch(quadrant)
+		{
+		//QUADRANT 1
+		case Q1:
+			//limit to top left of two arrays
+			quadrantRow = row/2;
+			quadrantColumn = column/2;
+			//loop through top left and send sums
+			//to sumArray
+			for(int i = 0; i < quadrantRow; i++)
+			{
+				for(int j = 0; j < quadrantColumn; j++)
+				{
+					int temp = A[i][j] + B[i][j];
+					sumArray[i][j] = temp;
+				}
+			}
+			break;
+		//QUADRANT 2
+		case Q2:
+			//limit to top right of the two arrays
+			quadrantColumn = column/2;
+			quadrantRow = row/2;
+			//loop through top right of the two arrays
+			//and send sums to sumArray
+			for(int i = 0; i < quadrantRow; i++)
+			{
+				for(int j = quadrantColumn; j < column; j++)
+				{
+					int temp = A[i][j] + B[i][j];
+					sumArray[i][j] = temp;
+				}
+			}
+			break;
+		//QUADRANT 3	
+		case Q3:
+			//odd rows
+			if(row % 2 != 0)
+			{
+				//if the rows are odd quadrant 3
+				//will have one more row than quadrants
+				//one and two
+				quadrantRow = (row/2) + 1;
+			}
+			else
+			{
+				quadrantRow = row;
+			}
+			quadrantColumn = column/2;
+			//loop through bottom left of the two arrays
+			//and add the sums to sumArray
+			for(int i = row/2; i < row; i++)
+			{
+				for(int j = 0; j < quadrantColumn; j++)
+				{
+					int temp = A[i][j] + B[i][j];
+					sumArray[i][j] = temp;
+				}
+			}
+			break;
+		//QUADRANT 4	
+		case Q4:
+			//odd row
+			if(row % 2 != 0)
+			{
+				//if the rows are odd quadrant 3
+				//will have one more row than quadrants
+				//one and two
+				quadrantRow = (row/2) + 1;
+			}
+			else
+			{
+				quadrantRow = row;
+			}
+			quadrantColumn = column/2;
+			//loop through bottom right of the two arrays
+			//and add their sums to sumArray
+			for(int i = row/2; i < row; i++)
+			{
+				for(int j = quadrantColumn; j < column; j++)
+				{
+					int temp = A[i][j] + B[i][j];
+					sumArray[i][j] = temp;
+					//Main.print2DArray(this.sumArray, row, column);
+					//System.out.println();
+					//try {Thread.sleep(100);} catch(Exception e) {}
+				}
+			}
+			break;
+		}//END of switch
+	}//END of run
+}//END of ThreadOperation
